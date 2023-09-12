5fcddbd51733fecf422b34b0d5c9872d96359454
Sujin Lee <sujinbonnaud@gmail.com>
2023-09-11 22:57:00 -0500

    brain storming basic structure of how this project works

--- project.js
+++ project.js
@@ -1,1 +1,7 @@
-fg
+// 1. Depot some money
+// 2. Determine number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. Check if the user won
+// 6. Give the user their winnings
+// 7. Play again.
