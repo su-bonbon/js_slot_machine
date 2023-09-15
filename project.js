b19a64b92cb252b65da81e42938b51af8151bf88
Sujin Lee <sujinbonnaud@gmail.com>
2023-09-14 21:07:12 -0500

    add input validation for user's input

--- project.js
+++ project.js
@@ -14,6 +14,10 @@ const prompt = require("prompt-sync")();
 const deposit = () => {
     const depositAmount = prompt("Enter a deposit amount: ");
     const numberDepositAmount = parseFloat(depositAmount);
+
+    if((isNaN(numberDepositAmount) || (numberDepositAmount <= 0)) {
+        console.log("Invalid deposit amount, try again")
+    }
 }
 
 deposit();
