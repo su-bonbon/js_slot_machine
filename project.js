6d01b11f232fdb950b4d563bfb608a1101c81eaf
Sujin Lee <sujinbonnaud@gmail.com>
2023-09-20 21:04:14 -0500

    create global variables

--- project.js
+++ project.js
@@ -8,6 +8,25 @@
 
 
 const prompt = require("prompt-sync")();
+
+
+const ROWS = 3;
+const COLS = 3;
+
+//this works as switch cases
+const SYMBOLS_COUNT = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+
+
+
+
+
+
 // function deposit() {
 //     return 1
 // }
